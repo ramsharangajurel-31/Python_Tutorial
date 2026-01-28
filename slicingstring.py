28db6d69af6183b96ef28bd3b65c06bda845367b
Ram Sharan Gajurel <ramsharangajurel9742@gmail.com>
2026-01-28 10:17:16 +0545

    slicing

---
+++ slicingstring.py
@@ -0,0 +1,15 @@
+# slicing with start and end
+str1 = "RamSharan"
+print(str1[0:5])
+print(str1[:5])
+print(str1[:])
+"'whole string '"
+
+print(str1[3:])
+print(str1[2:5])
+
+# syntax of slicing
+# string[ start:end:step]
+# default step :1
+# ending index = exclusive
+# starting index = inclusive
