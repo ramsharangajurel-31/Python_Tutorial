7b850817f0ef5c54c50bfbd39a0c3e4dcf43abc1
Ram Sharan Gajurel <ramsharangajurel9742@gmail.com>
2026-01-28 10:03:24 +0545

    Updated Indexing

---
+++ positivenegative indexing.py
@@ -0,0 +1,29 @@
+# positive indexing
+str1 = "RamSharan GAjurel"
+print (str1[0])
+print (str1[1])
+print (str1[2])
+print (str1[3])
+print (str1[4])
+print (str1[5])
+print(str1[6])
+print(str1[7])
+print(str1[8])
+print(str1[9])
+print(str1[10])
+
+print("-------------------------------------------")
+
+
+# negative indexing
+str2 = "RamSharan Gajurel"
+print (str2[-1])
+print (str2[-2])
+print (str2[-3])
+print (str2[-4])
+print (str2[-5])
+print (str2[-6])
+print (str2[-7])
+print (str2[-8])
+print (str2[-9])
+print (str2[-10])
---
+++ string.py
@@ -0,0 +1,14 @@
+# single quoted string
+str1 = 'Hello World'
+
+# double quoted string
+str2= "Hello World"
+
+# triple  single quoted string
+str3 = "'Hello World'"
+
+# triple double quoted string
+str4= """Hello World"""
+
+
+
