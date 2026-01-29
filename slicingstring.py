603203905c564b403c8f8650fe320db36ed04748
Ram Sharan Gajurel <ramsharangajurel9742@gmail.com>
2026-01-29 09:08:50 +0545

    Update

--- slicingstring.py
+++ slicingstring.py
@@ -14,7 +14,7 @@ print(str1[2:5])
 # ending index = exclusive
 # starting index = inclusive
 
-print("----------------------------------------------")
+print("=================================================")
 
 
 # slicing with negative indices
@@ -29,3 +29,54 @@ str3 = "RamSharan"
 print(str3[::2])
 print(str3[1::2])
 print(str3[::-1])
+
+print ("==================================================")
+
+ # slicing
+str4= "Hello World"
+first_word=str4[:5]
+print(first_word)
+
+print("======================================================")
+ #  reversed
+s="Hello World"
+reversed_s=s[::-1]
+print(reversed_s)
+
+print("======================================")
+
+ # every third character
+str5="Hello World"
+every_third_char=str5[-3:]
+print(every_third_char)
+
+print("=========================================")
+
+# last word
+str6="Hello World"
+last_word=str6[:7]
+print(last_word)
+
+# every second character
+str7="Hello World"
+every_second_char=str7[::2]
+print(every_second_char)
+
+print("==========================================")
+# others
+s="Hello World"
+# extract hello world using negative indices
+world=s[-6:-1]
+print(world)
+
+# extract "Hello"using a combination of positive slicing and step
+hello = s[0:5:1]
+print(hello)
+
+# reverse
+reversed_hello=s[4::-1]
+print(reversed_hello)
+
+
+
+
