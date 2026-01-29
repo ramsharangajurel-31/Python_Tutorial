e9ac9809776004193d1cb626905e25cc261867ee
Ram Sharan Gajurel <ramsharangajurel9742@gmail.com>
2026-01-29 09:39:12 +0545

    Update

--- IMP Questions/stringformatting.py
+++ IMP Questions/stringformatting.py
@@ -22,3 +22,14 @@ formatted_string1 = "Name:{0}, Age:{1}".format(name,age)
 print(formatted_string1)
 formatted_string2 = "Name:{name}, Age:{age}".format(name=name,age=age )
 print(formatted_string2)
+
+print("===================================================================")
+# third method
+# Formatting with String Literals, f-strings
+# syntax f"string(variable)"
+name = "RamSharan"
+age = 20
+formatted_string = f'Name:{name}, Age:{age}'
+print(formatted_string)
+formatted_string1= f"Next Year , {name} will be {age+1} years old"
+print(formatted_string1)
