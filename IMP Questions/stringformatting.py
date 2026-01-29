13fe4304b6e6c7b2e00459aac00b568d319b5f4a
Ram Sharan Gajurel <ramsharangajurel9742@gmail.com>
2026-01-29 09:18:30 +0545

    Updated

---
+++ IMP Questions/stringformatting.py
@@ -0,0 +1,11 @@
+# important questions
+
+#  String Formatting
+
+#  First Method
+# Formatting with the % operator
+# %s= string %d= integer
+name = 'ram'
+age = 20
+formatted_string = "Name : %s Age : %d"%(name,age)
+print(formatted_string)
