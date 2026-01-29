f5c9b779a23675c4d80fce741a252e93cd1b5f79
Ram Sharan Gajurel <ramsharangajurel9742@gmail.com>
2026-01-29 09:29:55 +0545

    Updated

--- IMP Questions/stringformatting.py
+++ IMP Questions/stringformatting.py
@@ -3,9 +3,22 @@
 #  String Formatting
 
 #  First Method
-# Formatting with the % operator
+# Formatting with the % operator--------------------------------
 # %s= string %d= integer
-name = 'ram'
+name = 'RamSharan'
 age = 20
-formatted_string = "Name : %s Age : %d"%(name,age)
+formatted_string = "Name : %s , Age : %d"%(name,age)
 print(formatted_string)
+
+print("===============================================================")
+# second method
+# Formatting with format() string Method ------------------------------
+#  Syntax "string{}" . format(value)
+name = "RamSharan"
+age = 20
+formatted_string = "Name:{}, Age:{}".format(name,age)
+print(formatted_string)
+formatted_string1 = "Name:{0}, Age:{1}".format(name,age)
+print(formatted_string1)
+formatted_string2 = "Name:{name}, Age:{age}".format(name=name,age=age )
+print(formatted_string2)
