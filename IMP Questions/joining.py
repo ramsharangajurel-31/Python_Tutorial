212a1f3ba0fcbf4b6827d765086a8eb2edd3ee47
Ram Sharan Gajurel <ramsharangajurel9742@gmail.com>
2026-02-12 10:02:17 +0545

    Updated

---
+++ IMP Questions/joining.py
@@ -0,0 +1,11 @@
+# joining
+# using the + operator
+t1 = (1,2,3)
+t2 = (4,5,6)
+joined = t1 + t2
+print(joined)
+
+# using sum with a list of tuples
+tuples_list =[(1,2),(3,4),(5,6)]
+joined_tuple = tuple(tuples_list,())
+print(joined_tuple)
