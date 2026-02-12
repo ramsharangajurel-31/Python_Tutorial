e9ca9cec5d0c444ee768771a82bfffcbc03dc038
Ram Sharan Gajurel <ramsharangajurel9742@gmail.com>
2026-02-12 10:08:05 +0545

    Updated

--- IMP Questions/joining.py
+++ IMP Questions/joining.py
@@ -9,3 +9,15 @@ print(joined)
 tuples_list =[(1,2),(3,4),(5,6)]
 joined_tuple = sum(tuples_list,())
 print(joined_tuple)
+# nested tuple joining
+t1=[1,2,3]
+t2=[4,5,6]
+nested_tuple = (t1,t2)
+print(nested_tuple)
+
+# using a loop to join tuples
+tuples_list =[(1,2),(3,4),(5,6)]
+joined_tuple =()
+for t in tuples_list:
+    joined_tuple +=t
+print(joined_tuple)
