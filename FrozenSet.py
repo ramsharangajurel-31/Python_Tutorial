1ce222e497765ecd3f6574d6559b3703370d36e7
Ram Sharan Gajurel <ramsharangajurel9742@gmail.com>
2026-02-13 07:30:02 +0545

    Updated

---
+++ FrozenSet.py
@@ -0,0 +1,12 @@
+# creating frozenset
+frozenset1 = frozenset([1,2,3,4,5])
+frozenset2 = frozenset([4,5,6,7,8])
+frozenset3 = frozenset("hello")
+print(frozenset1)
+print(frozenset2)
+print(frozenset3)
+
+# checking the types of a frozenset
+print(type(frozenset1))
+print(type(frozenset2))
+print(type(frozenset3))
---
+++ IMP Questions/Dictionary.py
@@ -0,0 +1,12 @@
+# using curly braces
+my_dict = {
+    "name" : "Ram",
+    "age" : 25,
+    "city" : "New York"
+}
+print(my_dict)
+print(type(my_dict))
+
+# uing the dict() function
+my_dict = dict(name="Ram", age=25, city="New York")
+print(my_dict)
